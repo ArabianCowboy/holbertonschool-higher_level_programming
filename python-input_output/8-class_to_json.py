7d32c0ae7b70c21f914992015e60cc8db1a98eac
arabian <MIDOXP@LIVE.COM>
2025-12-21 15:00:37 +0000

    Task 8: convert class instance to dictionary for JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to return the dictionary
+description of an object for JSON serialization.
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description of an object
+    for JSON serialization.
+    """
+    return obj.__dict__
