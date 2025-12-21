6e32145d53a3f359d439b51f2c10c72f907cf428
arabian <MIDOXP@LIVE.COM>
2025-12-21 15:02:49 +0000

    Task 11: complete Student JSON serialization and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes listed in attrs
+        that exist in the instance are returned.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            data = {}
+            for a in attrs:
+                if hasattr(self, a):
+                    data[a] = getattr(self, a)
+            return data
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance using a dictionary.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
