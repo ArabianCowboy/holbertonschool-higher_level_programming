d9b57771af060b502c419d8f249a88b304875178
arabian <midoxp@live.com>
2026-01-06 22:41:06 +0000

    Add simple_delete function

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module that deletes a key in a dictionary
+"""
+
+
+def simple_delete(a_dictionary, key=""):
+    """Delete a key in a dictionary if it exists."""
+    a_dictionary.pop(key, None)
+    return a_dictionary
