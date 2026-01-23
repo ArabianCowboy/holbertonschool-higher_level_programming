baa4e53e536017232f0cb551d315d2a6c3d81446
arabian <midoxp@gmail.com>
2026-01-23 16:48:36 +0300

    Task 8: list California cities using subquery

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,11 @@
--- TASK8
+-- 8. Cities of California
+-- List all cities of California without using JOIN
 
-SELECT id, name FROM cities
-WHERE stat_id = (
-	SELECT ID
-	FROM states
-	WHERE NAME ='California'
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
 )
 ORDER BY id ASC;
