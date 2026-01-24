b9dd59227eafc77bf62f859295f5965ed7cb1a92
arabian <midoxp@gmail.com>
2026-01-24 23:30:44 +0300

    Refactor Square class documentation for clarity and correctness

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""Define a square class"""
+
+
 class Square:
-    """Square class"""
-
+    """
+    Represents a Square class
+    """
     def __init__(self, size=0):
-        """Initialize square"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        """
+        Insitializes a new Square
 
+        :param size (int): its the size of the square, and the Default is 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
