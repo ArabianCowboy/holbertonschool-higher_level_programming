a44d05e92da6d9bafc6ba07d48ebaeba8fa23275
arabian <midoxp@gmail.com>
2026-02-19 01:38:47 +0300

    fix: refactor float validation logic in add_integer function for clarity

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,9 +16,19 @@ def add_integer(a, b=98):
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
-    if isinstance(a, float) and (a != a or a == float('inf') or a == float('-inf')):
-        raise ValueError("cannot convert float infinity to integer" if a == float('inf') or a == float('-inf') else "cannot convert float NaN to integer")
-    if isinstance(b, float) and (b != b or b == float('inf') or b == float('-inf')):
-        raise ValueError("cannot convert float infinity to integer" if b == float('inf') or b == float('-inf') else "cannot convert float NaN to integer")
+    if isinstance(a, float):
+        if a != a:
+            raise ValueError(
+                "cannot convert float NaN to integer")
+        if a == float('inf') or a == float('-inf'):
+            raise ValueError(
+                "cannot convert float infinity to integer")
+    if isinstance(b, float):
+        if b != b:
+            raise ValueError(
+                "cannot convert float NaN to integer")
+        if b == float('inf') or b == float('-inf'):
+            raise ValueError(
+                "cannot convert float infinity to integer")
 
     return int(a) + int(b)
